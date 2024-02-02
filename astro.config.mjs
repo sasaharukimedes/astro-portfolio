3973a9f8197e944938779333c3d662e98c8d61c9
sasaharukimedes <sasaharu3@gmail.com>
2024-02-02 20:13:37 +0900

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte()],
   output: "server",
   adapter: vercel(),
 });
