20649eff8ef3b4de325f61649ad1812f9bbc350a
sasaharukimedes <sasaharu3@gmail.com>
2024-02-02 21:55:52 +0900

    remove vercel and vscode

--- .vercel/output/functions/_render.func/home/sasaharukimedes/apps/astro-portfo/.vercel/output/_functions/chunks/generic_yeph5kFp.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_re-SO5hc.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/home/sasaharukimedes/apps/astro-portfo/.vercel/output/_functions/chunks/index_PuL7V1nu.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_6SQ2QUnj.mjs');
-
-export { page };
