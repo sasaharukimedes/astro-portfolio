fe232fe797f34b03a3576ab4662cc5348d91474e
sasaharukimedes <sasaharu3@gmail.com>
2024-02-02 20:07:12 +0900

    remove svelte from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind()]
   output: "server",
   adapter: vercel(),
 });
