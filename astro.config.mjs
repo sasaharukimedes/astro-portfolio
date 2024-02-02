892a6a7906610bbb832923c78f6e6633e0b01d33
sasaharukimedes <sasaharu3@gmail.com>
2024-02-02 20:04:25 +0900

    package reupdate, then remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
