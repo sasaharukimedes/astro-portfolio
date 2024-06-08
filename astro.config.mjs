ee00022f979c0c31df49461ad313c441ec97c7e4
sasaharukimedes <sasaharu3@gmail.com>
2024-06-08 16:19:10 +0900

    fix Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), icon()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
