35d4ff62c699b73420ecc3dd7b8eab1cde1dd2a8
hr-sasaki-haruki <sasaki@heartrails.com>
2025-01-12 16:50:04 +0900

    rewrite like a profile_book

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import svelte from "@astrojs/svelte";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), icon()],
+  integrations: [tailwind(), icon()],
   output: "server",
   adapter: vercel()
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway Variable', 'sans-serif'],
+        kiwimaru: ['Kiwimaru', 'serif'],
+        kurenaido: ['Zen Kurenaido', 'serif'],
+        homemade: ['Homemade Apple', 'serif'],
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    function ({ addUtilities }) {
+      addUtilities({
+        ".kurenaido-underline": {
+          fontFamily: "'Zen Kurenaido', serif",
+          textDecoration: "underline",
+          textUnderlineOffset: "5px",
+        },
+      });
+    },
+  ],
   daisyui: {
-    themes: ["autumn", "luxury", "wireframe", "lofi", "cyberpunk"],
+    themes: ["cupcake", "pastel", "retro", "valentine"],
   },
 };
